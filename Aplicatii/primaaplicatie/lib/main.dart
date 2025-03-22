053cdcd9f1617b49f8217426585e944b19343d15
AndreiTurcu1 <andrei.turcu@student.tuiasi.ro>
2025-03-22 20:08:41 +0200

    step-2

--- Aplicatii/primaaplicatie/lib/main.dart
+++ Aplicatii/primaaplicatie/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:primaaplicatie/views/login_view.dart';
 import 'package:primaaplicatie/views/register_view.dart';
+import 'package:primaaplicatie/views/verify_email_view.dart';
 
 import 'firebase_options.dart';
 
@@ -36,7 +37,17 @@ class HomePage extends StatelessWidget {
         builder: (context, snapshot){
           switch (snapshot.connectionState){
             case ConnectionState.done:
-            // final user = FirebaseAuth.instance.currentUser;
+              final user = FirebaseAuth.instance.currentUser;
+              if(user != null){
+                if(user.emailVerified){
+                  print('Email is verified');
+                } else {
+                  return const VerifyEmailView();
+                }
+              } else {
+                return const LoginView();
+              }
+              return const Text('Done');
             // if(user?.emailVerified ?? false){
             //   return const Text('Done');
             // } else {
